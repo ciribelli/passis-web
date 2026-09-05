1f7f330806e8da4bdec8ce7571626c8b788da43e
Otávio <otavio.ciribelli@gmail.com>
2026-09-05 15:07:05 -0300

    feat: include api key in requests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+if (process.env.REACT_APP_PASSIS_API_KEY) {
+  axios.defaults.headers.common['X-API-Key'] = process.env.REACT_APP_PASSIS_API_KEY;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
